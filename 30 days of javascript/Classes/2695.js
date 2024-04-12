2159ff5deb7936169f68cfa5b092008b889e4e4f
Gayathri Perumal <gayathriperumal100@gmail.com>
2024-04-12 17:12:18 +0530

    Array Wrapper

---
+++ 30 days of javascript/Classes/2695.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums
+ * @return {void}
+ */
+var ArrayWrapper = function(nums) {
+    this.nums = nums;
+};
+
+/**
+ * @return {number}
+ */
+ArrayWrapper.prototype.valueOf = function() {
+    if(this.nums.length === 0) {
+        return 0
+    } else {
+        let sum = this.nums.reduce((acc=0,num) => acc+num);
+        return sum
+    }
+}
+
+/**
+ * @return {string}
+ */
+ArrayWrapper.prototype.toString = function() {
+    return `[${this.nums}]`
+}
+
+/**
+ * const obj1 = new ArrayWrapper([1,2]);
+ * const obj2 = new ArrayWrapper([3,4]);
+ * obj1 + obj2; // 10
+ * String(obj1); // "[1,2]"
+ * String(obj2); // "[3,4]"
+ */
