1aa51f422d9345e6960342649524fe89553f532c
Gayathri Perumal <gayathriperumal100@gmail.com>
2024-04-12 16:50:11 +0530

    Counter

---
+++ 30 days of javascript/closures/2620.js
@@ -0,0 +1,11 @@
+var createCounter = function(n) {
+    let counter = n;
+    return function() {
+        return counter++;
+    };
+};
+
+const counter = createCounter(10)
+counter() // 10
+counter() // 11
+counter() // 12
